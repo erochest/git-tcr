a0b1d2e320969e0b96b836f2cad12d46452ccecd
Eric Rochester <eric.rochester@willowtreeapps.com>
2019-10-08 21:41:10 -0400

    working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{
+    app_from_crate, crate_authors, crate_description, crate_name, crate_version, App, Arg,
+    ArgMatches,
+};
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn build_app<'a, 'b>() -> App<'a, 'b> {
+    app_from_crate!().arg(
+        Arg::with_name("command")
+            .short("c")
+            .long("command")
+            .value_name("COMMAND")
+            .help("The command to run."),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spectral::prelude::*;
+
+    #[test]
+    fn test_app_takes_command() {
+        let app = build_app();
+        let args = app.get_matches_from(&["git-tcr", "--command=echo hello world"]);
+        assert_that(&args.value_of("command"))
+            .is_some()
+            .is_equal_to(&"echo hello world");
+    }
 }
